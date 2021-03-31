603bbd1f35c98636524570027af2e07c52f6d929
purple_rain <purplerain968@gmail.com>
2021-03-31 14:11:51 +0300

    Revert "Feature/4 add login and registration tests"

--- auth/constants.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-
-class AuthErrors(Enum):
-    auth_required = 'Authentication token required.'
-    invalid_token = 'Token is invalid.'
-    invalid_credentials = 'Invalid credentials.'
--- core/enums.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-
-class EnvironmentEnum(Enum):
-    dev = 'dev'
-    test = 'test'
