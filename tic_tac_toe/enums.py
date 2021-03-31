9986964d14cba8d2a2ddc7654803eadd469d5b13
Artem F <purplerain968@gmail.com>
2021-03-31 15:23:13 +0300

    feature/5: Provide a separate service to manipulate with a game.

--- tic_tac_toe/enums.py
+++ tic_tac_toe/enums.py
@@ -6,3 +6,9 @@ class GameResultEnum(enum.Enum):
     draft = 1
     player = 2
     computer = 3
+
+
+class TicTacToeErrors(enum.Enum):
+    invalid_turn = 'Invalid turn.'
+    game_already_over = 'The game is already over.'
+    not_user_game = 'This is not your game.'
